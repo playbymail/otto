b1ed9fc95f1b80a4838f512e661d44b925fccc8b
Michael Henderson <mdhender@mdhender.com>
2025-07-30 23:29:40 -0600

    Bump version to 0.8.0

--- version.go
+++ version.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	version = semver.Version{Minor: 7, Patch: 1, Build: semver.Commit()}
+	version = semver.Version{Minor: 8, Patch: 0, Build: semver.Commit()}
 )
 
 func Version() semver.Version {
